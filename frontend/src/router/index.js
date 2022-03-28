f8681bfc98a069ea5b2f283f422ead839e494a4a
gwon <yunghun97@naver.com>
2022-03-28 11:44:56 +0900

    Refactor : 2203328 프론트 구조 변경

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Main from "@/views/MainPage.vue";
 import Search from "@/views/SearchPage.vue";
+import User from "@/views/user/UserPage.vue"
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: "/search",
     name: "Search",
     component: Search,
+  },
+  {
+    path: "/user",
+    name: "User",
+    component: User,
   },
 ];
 
