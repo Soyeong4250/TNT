1e54139e7b04fb5bdd800dbda01738e501a02fe5
이소영 <th42500@naver.com>
2022-03-30 21:58:47 +0900

    FIX : 220330 이소영 코드 수정

--- backend/src/main/java/com/example/demo/Service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Service;
-
-import org.springframework.stereotype.Service;
-
-import reactor.core.publisher.Mono;
-
-public interface UserService {
-	Mono<String> getUserNaverInfo(String token);
-}
